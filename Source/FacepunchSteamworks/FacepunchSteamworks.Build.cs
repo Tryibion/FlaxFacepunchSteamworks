6ad14f16c133dec009e48eaf7bc2bc8e079921b9
Chandler Cox <mr.chandlercox@gmail.com>
2025-08-19 19:24:33 -0500

    Add redist libs. use .net6.0 dlls.

--- Source/FacepunchSteamworks/FacepunchSteamworks.Build.cs
+++ Source/FacepunchSteamworks/FacepunchSteamworks.Build.cs
@@ -5,16 +5,18 @@ using Flax.Build.NativeCpp;
 
 public class FacepunchSteamworks : GameModule
 {
+    public override void Init()
+    {
+        base.Init();
+        
+        BuildNativeCode = true;
+    }
+
     /// <inheritdoc />
     public override void Setup(BuildOptions options)
     {
         base.Setup(options);
 
-        // Here you can modify the build options for your game module
-        // To reference another module use: options.PublicDependencies.Add("Audio");
-        // To add C++ define use: options.PublicDefinitions.Add("COMPILE_WITH_FLAX");
-        // To learn more see scripting documentation.
-        BuildNativeCode = false;
         
         Tags["Network"] = string.Empty;
         options.PublicDependencies.Add("Networking");
@@ -23,22 +25,29 @@ public class FacepunchSteamworks : GameModule
         options.ScriptingAPI.IgnoreMissingDocumentationWarnings = true;
 
         var facepunchLibsPath = Path.Combine(FolderPath, "..", "..", "Content", "FacepunchLibs");
+        var redistPath = Path.Combine(facepunchLibsPath, "redistributable_bin");
         switch (options.Platform.Target)
         {
             case TargetPlatform.Windows:
                 options.ScriptingAPI.FileReferences.Add(Path.Combine(facepunchLibsPath, "Facepunch.Steamworks.Win64.dll"));
                 options.DependencyFiles.Add(Path.Combine(facepunchLibsPath, "Facepunch.Steamworks.Win64.xml"));
                 //options.DependencyFiles.Add(Path.Combine(facepunchLibsPath, "Facepunch.Steamworks.Win64.pdb"));
+                options.Libraries.Add(Path.Combine(redistPath, "win64", "steam_api64.lib"));
+                options.DependencyFiles.Add(Path.Combine(redistPath, "win64", "steam_api64.dll"));
                 break;
             case TargetPlatform.Linux:
                 options.ScriptingAPI.FileReferences.Add(Path.Combine(facepunchLibsPath, "Facepunch.Steamworks.Posix.dll"));
                 options.DependencyFiles.Add(Path.Combine(facepunchLibsPath, "Facepunch.Steamworks.Posix.xml"));
                 //options.DependencyFiles.Add(Path.Combine(facepunchLibsPath, "Facepunch.Steamworks.Posix.pdb"));
+                options.Libraries.Add(Path.Combine(redistPath, "linux64", "libsteam_api64.so"));
+                options.DependencyFiles.Add(Path.Combine(redistPath, "linux64", "libsteam_api64.so"));
                 break;
             case TargetPlatform.Mac:
                 options.ScriptingAPI.FileReferences.Add(Path.Combine(facepunchLibsPath, "Facepunch.Steamworks.Posix.dll"));
                 options.DependencyFiles.Add(Path.Combine(facepunchLibsPath, "Facepunch.Steamworks.Posix.xml"));
                 //options.DependencyFiles.Add(Path.Combine(facepunchLibsPath, "Facepunch.Steamworks.Posix.pdb"));
+                options.Libraries.Add(Path.Combine(redistPath, "osx", "libsteam_api.dylib"));
+                options.DependencyFiles.Add(Path.Combine(redistPath, "osx", "libsteam_api.dylib"));
                 break;
             default: /*throw new InvalidPlatformException(options.Platform.Target)*/ break;
         }
